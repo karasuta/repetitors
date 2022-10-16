bdca6e1925bd93a28344b559b9752b0ac405aee5
karasuta <kargina.ts@gmail.com>
2022-10-16 20:32:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import{ data } from './data';
 import './App.css';
 
 function App() {
+  const [teacher, setTeacher] =useState(0);
+  const {name,subject,experience,image,description}=data[teacher];
+  const [showMore,setShowMore]=useState(false);
+  const nextTeacher = ()=>{
+    setTeacher((teacher=>{
+      teacher++;
+      if (teacher>data.length-1){
+        teacher=0;
+      }
+      setShowMore(false);
+      return teacher;
+    }))
+  }
+  const previousTeacher = ()=>{
+    setTeacher(teacher=>{
+      teacher--;
+      if (teacher<0){
+        teacher=data.length-1;
+      }
+      setShowMore(false);
+      return teacher;
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='title'>
+        <h1>Наши репетиторы</h1>
+      </div>
+      <div className='btn_container'>
+          <button className='btn' onClick={previousTeacher}>Назад</button>
+          <button className='btn' onClick={nextTeacher}>Вперёд</button>
+      </div>
+      <div className='main_container'>
+        <div className='container_photo'>
+          <img src={image} width='200px' alt='person'/>
+        </div>
+        <div className='container_info'>
+          <h2>{name}</h2>
+          <h3>Предмет: <span className='red'>{subject}</span></h3>
+          <h3>Опыт преподавания: {experience}</h3>
+          <p>{showMore?description:description.substring(0,150)+'...'}
+          <button className='show' onClick={()=>setShowMore(!showMore)}>{showMore?"скрыть":"подробнее"}</button>
+          </p>
+        </div>
     </div>
+  </div>
   );
 }
 
 export default App;
+
